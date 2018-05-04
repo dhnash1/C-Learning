0b58b1ecabafd1b5479a324a79a5f75ee2e2bd25
Deforis Nash <defhnash@gmail.com>
2018-05-04 12:01:19 -0500

    LEarned about using IOstream, and variables

---
+++ IOstream.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+cout << "Hey there" <<std::endl;
+
+int x = 5;
+int y;
+
+cout << "the number is " << x << " and thats pretty great" << std::endl << "This should be a new line" << std::endl;
+
+cout << "heres somne conditional operators!" << std::endl;
+
+y = (x == 5) ? 0 : 1;
+
+cout << "x equals 5?" << y << std::endl;
+
+
+
+
+return 0;
+}
